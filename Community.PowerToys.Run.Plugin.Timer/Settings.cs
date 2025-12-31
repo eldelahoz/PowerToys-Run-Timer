7d22033180fac8210ece3217c0b35795b289c39c
Andres De la Hoz <andelahoz13@gmail.com>
2025-12-31 17:05:08 -0500

    UPDATE: Generic settings for sound control

--- Community.PowerToys.Run.Plugin.Timer/Settings.cs
+++ Community.PowerToys.Run.Plugin.Timer/Settings.cs
@@ -5,31 +5,67 @@ namespace Community.PowerToys.Run.Plugin.Timers;
 
 public class Settings
 {
-    public static List<KeyValuePair<string, string>> TimeSpanParserConfigurationOptions
-        = new List<KeyValuePair<string, string>>()
-            {
-                new("None", ((int)Units.None).ToString()),
-                new("Milliseconds", ((int)Units.Milliseconds).ToString()),
-                new("Seconds", ((int)Units.Seconds).ToString()),
-                new("Minutes", ((int)Units.Minutes).ToString()),
-                new("Hours", ((int)Units.Hours).ToString()),
-                new("Days", ((int)Units.Days).ToString()),
-                new("Weeks", ((int)Units.Weeks).ToString()),
-                new("Months", ((int)Units.Months).ToString()),
-            };
-
-    public TimeSpanParserOptions TimeSpanParserOptions => new()
+    public static List<KeyValuePair<string, string>> TimeSpanParserConfigurationOptions = new List<
+        KeyValuePair<string, string>
+    >()
     {
-        UncolonedDefault = TimeSpanParserUncolonedDefault,
-        ColonedDefault = TimeSpanParserColonedDefault,
+        new("None", ((int)Units.None).ToString()),
+        new("Milliseconds", ((int)Units.Milliseconds).ToString()),
+        new("Seconds", ((int)Units.Seconds).ToString()),
+        new("Minutes", ((int)Units.Minutes).ToString()),
+        new("Hours", ((int)Units.Hours).ToString()),
+        new("Days", ((int)Units.Days).ToString()),
+        new("Weeks", ((int)Units.Weeks).ToString()),
+        new("Months", ((int)Units.Months).ToString()),
     };
+
+    public TimeSpanParserOptions TimeSpanParserOptions =>
+        new()
+        {
+            UncolonedDefault = TimeSpanParserUncolonedDefault,
+            ColonedDefault = TimeSpanParserColonedDefault,
+        };
 
     public Units TimeSpanParserColonedDefault { get; private set; } = Units.Hours;
     public Units TimeSpanParserUncolonedDefault { get; private set; } = Units.Minutes;
 
+    public bool EnableAlarmSound { get; private set; } = false;
+    public string CustomSoundPath { get; private set; } = "";
+    public int AlarmDurationSeconds { get; private set; } = 5;
+
     public void UpdateSettings(IEnumerable<PluginAdditionalOption> settings)
     {
-        TimeSpanParserUncolonedDefault = (Units)settings.Single(x => x.Key == nameof(Settings.TimeSpanParserUncolonedDefault)).ComboBoxValue;
-        TimeSpanParserColonedDefault = (Units)settings.Single(x => x.Key == nameof(Settings.TimeSpanParserColonedDefault)).ComboBoxValue;
+        TimeSpanParserUncolonedDefault = (Units)
+            settings
+                .Single(x => x.Key == nameof(Settings.TimeSpanParserUncolonedDefault))
+                .ComboBoxValue;
+        TimeSpanParserColonedDefault = (Units)
+            settings
+                .Single(x => x.Key == nameof(Settings.TimeSpanParserColonedDefault))
+                .ComboBoxValue;
+
+        var enableAlarmSetting = settings.FirstOrDefault(x =>
+            x.Key == nameof(Settings.EnableAlarmSound)
+        );
+        if (enableAlarmSetting is not null)
+        {
+            EnableAlarmSound = enableAlarmSetting.Value;
+        }
+
+        var customSoundSetting = settings.FirstOrDefault(x =>
+            x.Key == nameof(Settings.CustomSoundPath)
+        );
+        if (customSoundSetting is not null)
+        {
+            CustomSoundPath = customSoundSetting.TextValue ?? "";
+        }
+
+        var alarmDurationSetting = settings.FirstOrDefault(x =>
+            x.Key == nameof(Settings.AlarmDurationSeconds)
+        );
+        if (alarmDurationSetting is not null && alarmDurationSetting.NumberValue > 0)
+        {
+            AlarmDurationSeconds = (int)Math.Clamp(alarmDurationSetting.NumberValue, 1, 30);
+        }
     }
 }
