23777fc186d870343f9b164ef2d2ea921fd9b58a
Andres De la Hoz <andelahoz13@gmail.com>
2025-12-31 22:09:12 +0000

    ADD: Documentation sound functions

--- Community.PowerToys.Run.Plugin.Timer/Main.cs
+++ Community.PowerToys.Run.Plugin.Timer/Main.cs
@@ -12,6 +12,8 @@ public class Main : IPlugin, IContextMenu, ISettingProvider
     private TimerResultService? _timerService;
     private PluginInitContext? _context;
     private Settings _settings = new();
+    private SoundService? _soundService;
+    private string? _pluginDirectory;
 
     public string Name => "Timer";
 
@@ -19,44 +21,79 @@ public class Main : IPlugin, IContextMenu, ISettingProvider
 
     public static string PluginID => "0cd68088246649a38205c7641df39db0";
 
-    public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>()
+    public IEnumerable<PluginAdditionalOption> AdditionalOptions
     {
-        new PluginAdditionalOption()
+        get
         {
-            Key = nameof(Settings.TimeSpanParserUncolonedDefault),
-            DisplayLabel = "Uncoloned Time Configuration",
-            DisplayDescription = "Sets how the first number of the timer should be treated, when not using colons.",
-            ComboBoxValue = (int)_settings.TimeSpanParserUncolonedDefault,
-            PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Combobox,
-            ComboBoxItems = Settings.TimeSpanParserConfigurationOptions
-        },
-        new PluginAdditionalOption()
-        {
-            Key = nameof(Settings.TimeSpanParserColonedDefault),
-            DisplayLabel = "Coloned Time Configuration",
-            DisplayDescription = "Sets how the first number of the timer should be treated, when using colons (i.e. 4:30).",
-            ComboBoxValue = (int)_settings.TimeSpanParserColonedDefault,
-            PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Combobox,
-            ComboBoxItems = Settings.TimeSpanParserConfigurationOptions
-        },
-    };
+            return
+            [
+                new PluginAdditionalOption()
+                {
+                    Key = nameof(Settings.TimeSpanParserUncolonedDefault),
+                    DisplayLabel = "Uncoloned Time Configuration",
+                    DisplayDescription =
+                        "Sets how the first number of the timer should be treated, when not using colons.",
+                    ComboBoxValue = (int)_settings.TimeSpanParserUncolonedDefault,
+                    PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Combobox,
+                    ComboBoxItems = Settings.TimeSpanParserConfigurationOptions,
+                },
+                new PluginAdditionalOption()
+                {
+                    Key = nameof(Settings.TimeSpanParserColonedDefault),
+                    DisplayLabel = "Coloned Time Configuration",
+                    DisplayDescription =
+                        "Sets how the first number of the timer should be treated, when using colons (i.e. 4:30).",
+                    ComboBoxValue = (int)_settings.TimeSpanParserColonedDefault,
+                    PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Combobox,
+                    ComboBoxItems = Settings.TimeSpanParserConfigurationOptions,
+                },
+                new PluginAdditionalOption()
+                {
+                    Key = nameof(Settings.EnableAlarmSound),
+                    DisplayLabel = "Enable Alarm Sound",
+                    DisplayDescription =
+                        "Enable or disable sound notifications when a timer expires.",
+                    Value = _settings.EnableAlarmSound,
+                    PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Checkbox,
+                },
+                new PluginAdditionalOption()
+                {
+                    Key = nameof(Settings.CustomSoundPath),
+                    DisplayLabel = "Custom Sound Path",
+                    DisplayDescription =
+                        "Full path to a .wav file to use as alarm sound. Leave empty to use the built-in default sound.",
+                    TextValue = _settings.CustomSoundPath,
+                    PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Textbox,
+                },
+                new PluginAdditionalOption()
+                {
+                    Key = nameof(Settings.AlarmDurationSeconds),
+                    DisplayLabel = "Alarm Duration",
+                    DisplayDescription = "How many seconds the alarm will play (1-30 seconds).",
+                    NumberValue = _settings.AlarmDurationSeconds,
+                    PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Numberbox,
+                },
+            ];
+        }
+    }
 
     public void Init(PluginInitContext context)
     {
         ArgumentNullException.ThrowIfNull(context);
 
         _context = context;
-        _timerService = new(_context);
+        _pluginDirectory = _context.CurrentPluginMetadata.PluginDirectory;
+        _soundService = new(_pluginDirectory);
+        _timerService = new(_context, _soundService, _settings);
     }
 
-    public List<Result> Query(Query query)
-        => _timerService!.GetQueryResult(query, _settings);
+    public List<Result> Query(Query query) => _timerService!.GetQueryResult(query, _settings);
 
-    public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
-        => _timerService!.GetContextMenuResults(selectedResult);
+    public List<ContextMenuResult> LoadContextMenus(Result selectedResult) =>
+        _timerService!.GetContextMenuResults(selectedResult);
 
-    public System.Windows.Controls.Control CreateSettingPanel()
-        => throw new NotImplementedException();
+    public System.Windows.Controls.Control CreateSettingPanel() =>
+        throw new NotImplementedException();
 
     public void UpdateSettings(PowerLauncherPluginSettings settings)
     {
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <div align="center">
 
 # PowerToys Run: Timers
-  
+
 [![GitHub release](https://img.shields.io/github/v/release/CoreyHayward/PowerToys-Run-Timer?style=flat-square)](https://github.com/CoreyHayward/PowerToys-Run-Timer/releases/latest)
 [![GitHub all releases](https://img.shields.io/github/downloads/CoreyHayward/PowerToys-Run-Timer/total?style=flat-square)](https://github.com/CoreyHayward/PowerToys-Run-Timer/releases/)
 [![GitHub release (latest by date)](https://img.shields.io/github/downloads/CoreyHayward/PowerToys-Run-Timer/latest/total?style=flat-square)](https://github.com/CoreyHayward/PowerToys-Run-Timer/releases/latest)
@@ -25,20 +25,35 @@ Simple [PowerToys Run](https://learn.microsoft.com/windows/powertoys/run) plugin
 - Open PowerToys
 
 ## Usage
+
 ### Setting Timers
+
 - Open PowerToys Run
 - Input: "timer {time} {description (optional)}"
 - Select the result (ENTER)
 - Timer is set and notification is raised when timer is complete
 
 {time} can be specified in a range of formats thanks to [TimeSpanParser](https://github.com/pengowray/TimeSpanParser) some examples include:
+
 - Seconds: 15s | 15 sec(s) | 15 seconds
 - Minutes: 1m | 1 min | 1 minute
 - Hours: 1h | 1 hr | 1 hour
 
 ### Viewing Active Timers
+
 ![See active timers](/images/ActiveTimers.png)
+
 - Open PowerToys Run
 - Input: "timer"
 
 Timers can also be stopped by clicking the delete button or using the CTRL+ENTER shortcut on a running timer
+
+### Alarm Sound
+
+![Sound Options](/images/SoundOptions.png)
+
+- Enable Alarm Sound: You can hear the sound.
+- Custom Sound Path: If you want to customize the default sound, you can add your own; using .wav is suggested.
+- Alarm Duration: The time at which the sound will repeat
+
+Available sound options can be configured to customize your timer notifications.
